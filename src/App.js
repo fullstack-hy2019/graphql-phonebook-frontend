177db9c6ce859f70195624319fed68e6b369a855
Matti Luukkainen <mluukkai@iki.fi>
2019-02-17 19:39:28 +0200

    part81

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Query, ApolloConsumer, Mutation } from "react-apollo"
+import { gql } from 'apollo-boost'
+import Persons from './components/Persons'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const allPersons = gql`
+{
+  allPersons  {
+    name
+    phone
   }
 }
+`
+const App = () => {
+  return (
+    <div>
+      <ApolloConsumer>
+        {(client => 
+          <Query query={allPersons}>
+            {(result) => <Persons result={result} client={client} />}
+          </Query> 
+        )}
+      </ApolloConsumer>
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Persons.js
@@ -0,0 +1,56 @@
+import React,  {useState } from 'react'
+import { gql } from 'apollo-boost'
+
+const findPerson = gql`
+query findPersonByName($nameToSearch: String!) {
+  findPerson(name: $nameToSearch) {
+    name
+		phone 
+		address{
+      street
+      city
+    }
+  }
+}
+`
+
+const Persons = ({ result, client }) => {
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const [person, setPerson] = useState(null)
+
+  if (person) {
+    return(
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street} {person.address.city}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    )
+  }
+
+  const show = async (name) => {
+    const { data } = await client.query({
+      query: findPerson,
+      variables: { nameToSearch: name }
+    })
+    setPerson(data.findPerson)
+  }
+
+  return (
+    <div>
+      <h2>Persons</h2>
+      {result.data.allPersons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+          <button onClick={() => show(p.name)}>show address</button>
+        </div>  
+      )}
+    </div>
+  )
+}
+
+export default Persons
