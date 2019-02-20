16523309ed1b5012bf381f46c791ccdb1e402f6f
Matti Luukkainen <mluukkai@iki.fi>
2019-02-20 19:01:17 +0200

    part8-6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { gql } from 'apollo-boost'
 import { useQuery, useMutation } from 'react-apollo-hooks' 
+import { useApolloClient } from 'react-apollo-hooks'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const ALL_PERSONS = gql`
 {
@@ -49,14 +51,35 @@ mutation editNumber($name: String!, $phone: String!) {
 }
 `
 
+const LOGIN = gql`
+mutation login($username: String!, $password: String!) {
+  login(username: $username, password: $password)  {
+    value
+  }
+}
+`
+
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  useEffect(() => {
+    setToken(localStorage.getItem('library-user-token', token))
+  }, [])
 
   const handleError = (error) => {
     setErrorMessage(error.graphQLErrors[0].message)
     setTimeout(() => {
       setErrorMessage(null)
     }, 10000)
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const result = useQuery(ALL_PERSONS)
@@ -68,14 +91,33 @@ const App = () => {
 
   const editNumber = useMutation(EDIT_NUMBER)
 
+  const login = useMutation(LOGIN)
+
+  const errorNotification = () => errorMessage &&
+    <div style={{ color: 'red' }}>
+      {errorMessage}
+    </div>
+
+  if (!token) {
+    return (
+      <div>
+        {errorNotification()}
+        <h2>Login</h2>
+        <LoginForm
+          login={login}
+          setToken={(token) => setToken(token)}
+          handleError={handleError}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
-      {errorMessage &&
-        <div style={{ color: 'red' }}>
-          {errorMessage}
-        </div>
-      }
+      {errorNotification()}
       
+      <button onClick={logout}>logout</button>
+
       <Persons result={result} />
 
       <h2>create new</h2>
---
+++ src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const LoginForm = (props) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const result = await props.login({
+        variables: { username, password }
+      })
+
+      const token = result.data.login.value
+
+      props.setToken(token)
+      localStorage.setItem('library-user-token', token)
+    } catch(error){
+      props.handleError(error)
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
